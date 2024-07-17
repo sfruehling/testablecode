65e54d4f5dd733693a615e95850c910df17dbc8f
sfruehling <sebastian.fruehling@andrena.de>
2024-07-17 19:03:27 +0200

    initial commit

--- src/main/java/com/example/demo/SomeService.java
+++ src/main/java/com/example/demo/SomeService.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
 import com.example.demo.notsoimportant.Bar;
-import com.example.demo.notsoimportant.Baz;
+import com.example.demo.notsoimportant.BazWrapper;
 import com.example.demo.notsoimportant.Foo;
-import org.springframework.beans.factory.annotation.Autowired;
 
 class SomeService {
-    @Autowired
     private Foo foo;
+    private final Bar bar;
+    private final BazWrapper baz;
+
+    public SomeService(Foo foo, Bar bar, BazWrapper baz) {
+        this.foo = foo;
+        this.bar = bar;
+        this.baz = baz;
+    }
 
     public int someMethod() {
-        Bar bar = new Bar(42);
-        return foo.foo() + bar.bar() + Baz.magic();
+        return foo.foo() + bar.bar() +baz.getMagic();
     }
+
 }
