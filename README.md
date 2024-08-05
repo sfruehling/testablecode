be05e8b6124b42e1e2c44a41447425d7744da30d
sfruehling <sebastian.fruehling@andrena.de>
2024-08-05 15:39:30 +0200

    added demo1

---
+++ README.md
@@ -0,0 +1,6 @@
+weitere Ideen:
+* Test Exception handling
+* Test Logging
+* Test Filehandling (@TemDir)
+* Mocking
+* SpringBoot Test Knowhow
---
+++ src/main/java/com/example/demo1/MyService.java
@@ -0,0 +1,26 @@
+package com.example.demo1;
+
+import com.example.demo1.notsoimportant.MyDateFormatter;
+
+public class MyService {
+
+
+    private final MyDateFormatter myDateFormatter;
+
+    public MyService(MyDateFormatter myDateFormatter) {
+        this.myDateFormatter = myDateFormatter;
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    public String getResult(long timestamp1, long timestamp2) {
+
+        if (timestamp2 % 2 == 0) {
+            return myDateFormatter.convertToISO8601(timestamp1);
+        }
+
+        if (timestamp1 % 5 == 0) {
+            return myDateFormatter.convertToISO8601(timestamp2);
+        }
+        return myDateFormatter.convertToISO8601(timestamp1 + timestamp2);
+    }
+}
