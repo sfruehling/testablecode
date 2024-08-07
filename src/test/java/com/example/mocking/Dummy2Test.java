d086c9851bcebd7e2b7ce608dc4e29fa54378a6e
sfruehling <sebastian.fruehling@andrena.de>
2024-08-07 16:32:38 +0200

    fix tests

--- src/test/java/com/example/mocking/Dummy2Test.java
+++ src/test/java/com/example/mocking/Dummy2Test.java
@@ -2,9 +2,10 @@ package com.example.mocking;
 
 import com.example.mocking.notsoimportant.Foo;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class Dummy2Test {
 
@@ -14,14 +15,19 @@ class Dummy2Test {
     @Test
     void calculate_HasNoInteractionWithFoo() {
         dummy2.calculate();
-        // verify that dummy2.calculate() does not interact with mockFoo
+
+        verifyNoInteractions(mockFoo);
     }
 
     @Test
     void calculate2_Has2InteractionsWithFooMagicFirstIs2SecondIs3() {
+        InOrder inOrder = inOrder(mockFoo);
         dummy2.calculate2();
 
         // verify that dummy2.calculate2() interacts first with mockFoo.magic(2) and then with mockFoo.magic(3)
+        inOrder.verify(mockFoo).magic(2);
+        inOrder.verify(mockFoo).magic(3);
+
     }
 
     @Test
@@ -30,12 +36,14 @@ class Dummy2Test {
         dummy2.calculate3();
 
         // verify that dummy2.calculate3() interacts 5 times with mockFoo.magic(2)
+        verify(mockFoo, times(5)).magic(2);
     }
 
 
     @Test
     void calculate4_returns3OnArithmeticException() {
         // train foo.voidMethod() to throw ArithmeticException
+        doThrow(new ArithmeticException()).when(mockFoo).voidMethod();
 
         assertThat(dummy2.calculate4()).isEqualTo(3);
     }
--- src/test/java/com/example/mocking/DummyTest.java
+++ src/test/java/com/example/mocking/DummyTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class DummyTest {
 
@@ -15,6 +15,7 @@ class DummyTest {
 
     @Test
     void simpleExample() {
+        when(fooMock.tweakInteger(123)).thenReturn(100);
         int result = dummy.getInteger();
         assertThat(result).isEqualTo(100);
     }
@@ -22,6 +23,7 @@ class DummyTest {
     @Test
     void hiddenSideEffect() {
         // make this test run in just a few milliseconds
+        when(fooMock.getTimeToWait()).thenReturn(1L);
         String result = dummy.run();
         assertThat(result).isEqualTo("OK");
     }
@@ -30,17 +32,22 @@ class DummyTest {
     void verification() {
         dummy.getInteger();
         // verify that Foo.echoInteger has been called with value 123
+        verify(fooMock).tweakInteger(123);
     }
 
     @Test
     void hardToReproduceResult() {
         // make the test pass by mocking the "magic" method twice, depending on the input
+        when(fooMock.magic(1)).thenReturn(2);
+        when(fooMock.magic(2)).thenReturn(3);
+
         int result = dummy.doMagic(1, 2);
         assertThat(result).isEqualTo(42);
     }
 
     @Test
     void impossibleToReproduceException() {
+        when(fooMock.tweakInteger(123)).thenThrow(new ArithmeticException());
         assertThatExceptionOfType(IllegalStateException.class)
                 .isThrownBy(() -> dummy.getInteger());
     }
